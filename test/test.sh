a1c5764053fad67a737a83c137136191f04186ea
take-cheeze <takechi101010@gmail.com>
2019-02-11 10:19:53 +0000

    Build lua-5.3 on linux

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,18 @@ set -euxo pipefail
 root_dir="$(cd "${BASH_SOURCE[0]%/*}/.." && pwd)"
 cd "$root_dir"
 
+COMPAT_LUA_VERSION=5.3.4
+
+if [ $(uname -s) = 'Linux' ] ; then
+    wget --continue "https://www.lua.org/ftp/lua-${COMPAT_LUA_VERSION}.tar.gz"
+    tar xf lua-${COMPAT_LUA_VERSION}.tar.gz
+    pushd lua-${COMPAT_LUA_VERSION}
+    make -j$(nproc) linux
+    popd
+    export PATH="$root_dir/lua-${COMPAT_LUA_VERSION}/src:$PATH"
+fi
+
 go install github.com/Azure/golua/cmd/glua
 
-cd "test/lua-5.3.4"
+cd "test/lua-$COMPAT_LUA_VERSION"
 glua -tests all.lua | grep -q OK
